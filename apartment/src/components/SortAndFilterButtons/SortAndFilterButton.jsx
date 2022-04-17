c9531ab02d7336ddda8bbbf62bc7ceb454ed0c72
Vigneshprabu1398 <76090127+Vigneshprabu1398@users.noreply.github.com>
2022-04-17 19:39:27 +0530

    updated selector

--- apartment/src/components/SortAndFilterButtons/SortAndFilterButton.jsx
+++ apartment/src/components/SortAndFilterButtons/SortAndFilterButton.jsx
@@ -1,14 +1,28 @@
 import './Sort.css'
 
 export const SortAndFilterButtons = ({handleSort})=>{
+    const options = [
+        {
+            label: "---",
+            value: "---",
+          },
+        {
+          label: "Owner",
+          value: "owner",
+        },
+        {
+          label: "Tenent",
+          value: "tenent",
+        }
+    ]
     return (
         <div>
-                <select name="" className="filter"
-                onClick={() => handleSort("country", 1)}>
-                    <option value="">Select</option>
-                    <option value="owner">Owner</option>
-                    <option value="tenant">Tenant</option>
-                </select>
+            <label><b>Select Resident Type :</b></label>
+               <select>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
 
 
                 <button className="asc"
